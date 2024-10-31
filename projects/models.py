2cfcd0f361d6f40dead7c7e8419cf1f45c9c9e84
Gustavo Larco <gustavo.larcoj@gmail.com>
2024-10-30 21:06:16 -0500

    First commit and model

---
+++ projects/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=15)
+    address = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.address}"
